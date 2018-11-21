c257ba908ab96865c73b6d9af330db464c2ff65a
Mohammad Tasleem Arif <mohammadtasleemarif@Mohammads-MacBook-Air.local>
2018-11-22 02:07:07 +0530

    Fixed material CSS.

--- job-portal-app/src/app/home/home.module.ts
+++ job-portal-app/src/app/home/home.module.ts
@@ -1,25 +1,30 @@
-import { CommonComponentModule } from './../common/common-component.module';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HomeRoutingModule } from './home-routing.module';
+import { FormsModule } from '@angular/forms';
+import { AppMaterialModule } from './../app-material/app-material.module';
+import { CommonComponentModule } from './../common/common-component.module';
+
 import { HomeComponent } from './home.component';
 import { PostedJobsComponent } from './posted-jobs/posted-jobs.component';
 import { ShortListedComponent } from './short-listed/short-listed.component';
 import { InterviewsComponent } from './interviews/interviews.component';
-import { MatTableModule } from '@angular/material';
+import { SearchComponent } from './search/search.component';
 
 @NgModule({
   declarations: [
     HomeComponent,
     PostedJobsComponent,
     ShortListedComponent,
-    InterviewsComponent
+    InterviewsComponent,
+    SearchComponent
   ],
   imports: [
     CommonModule,
     HomeRoutingModule,
     CommonComponentModule,
-    MatTableModule
+    AppMaterialModule,
+    FormsModule
   ],
   exports: [
     HomeComponent
